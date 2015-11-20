05d114c5625631a50dd11d6a4b8c02480b0bf075
designaroni <ian.roberts@sportngin.com>
2015-11-20 16:26:25 -0600

    modified front matter to have body class, commented out plugin file, added details to config

--- _plugins/bodyClass.rb
+++ _plugins/bodyClass.rb
@@ -1,37 +1,37 @@
-# src url http://joe8bit.com/article/jekyll-generate-body-class/
-
-class BodyClassTag < Liquid::Tag
-
-  def generate_body_class(prefix, id)
-    id = id.gsub(/\.\w*?$/, '').gsub(/[-\/]/, '_').gsub(/^_/, '') # Remove extension from url, replace '-' and '/' with underscore, Remove leading '_'
-
-    case prefix
-    when "class"
-      prefix = ""
-    else
-      prefix = "#{prefix}_"
-    end
-
-    # "#{prefix}#{id}"
-    "#{id}"
-  end
-
-  def render(context)
-    page = context.environments.first["page"]
-    classes = []
-
-    %w[class url categories tags layout].each do |prop|
-      next unless page.has_key?(prop)
-      if page[prop].kind_of?(Array)
-        page[prop].each { |proper| classes.push generate_body_class(prop, proper) }
-      else
-        classes.push generate_body_class(prop, page[prop])
-      end
-    end
-
-    classes.join(" ")
-  end
-
-end
-
-Liquid::Template.register_tag('body_class', BodyClassTag)
+# # src url http://joe8bit.com/article/jekyll-generate-body-class/
+#
+# class BodyClassTag < Liquid::Tag
+#
+#   def generate_body_class(prefix, id)
+#     id = id.gsub(/\.\w*?$/, '').gsub(/[-\/]/, '_').gsub(/^_/, '') # Remove extension from url, replace '-' and '/' with underscore, Remove leading '_'
+#
+#     case prefix
+#     when "class"
+#       prefix = ""
+#     else
+#       prefix = "#{prefix}_"
+#     end
+#
+#     # "#{prefix}#{id}"
+#     "#{id}"
+#   end
+#
+#   def render(context)
+#     page = context.environments.first["page"]
+#     classes = []
+#
+#     %w[class url categories tags layout].each do |prop|
+#       next unless page.has_key?(prop)
+#       if page[prop].kind_of?(Array)
+#         page[prop].each { |proper| classes.push generate_body_class(prop, proper) }
+#       else
+#         classes.push generate_body_class(prop, page[prop])
+#       end
+#     end
+#
+#     classes.join(" ")
+#   end
+#
+# end
+#
+# Liquid::Template.register_tag('body_class', BodyClassTag)
